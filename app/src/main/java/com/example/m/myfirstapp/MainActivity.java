f3956ad522a6bb79e1a1c9a59a752273fd439f4e
MJJoker <ziebell_marco@posteo.de>
2018-03-03 10:21:10 +0100

    cyclicly pulling the PM2.5, PM10, Wifi(db) and Wifi(%)

--- app/src/main/java/com/example/m/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/m/myfirstapp/MainActivity.java
@@ -1,10 +1,17 @@
 package com.example.m.myfirstapp;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.util.regex.Matcher;
@@ -14,74 +21,147 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
-    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+import static com.example.m.myfirstapp.MyIntentService.EXTRA_PARAM_PM10;
+import static com.example.m.myfirstapp.MyIntentService.EXTRA_PARAM_PM25;
+import static com.example.m.myfirstapp.MyIntentService.EXTRA_PARAM_WIFIDB;
+import static com.example.m.myfirstapp.MyIntentService.EXTRA_PARAM_WIFIPERCENT;
 
-    int counter = 0;
-    OkHttpClient client = new OkHttpClient();
+public class MainActivity extends AppCompatActivity {
+
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    public static final int MSG_UNCOLOR_START = 0;
+    public static final int MSG_UNCOLOR_STOP = 1;
+    public static final int MSG_COLOR_START = 2;
+    public static final int MSG_COLOR_STOP = 3;
+    private static OkHttpClient client;
+
+
+    private CountDownTimer downcounter;
+
+    // handler for received data from service
+    private final BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            if (intent.getAction().equals(MyIntentService.BROADCAST_ACTION_BAZ)) {
+                final String param_pm25 = intent.getStringExtra(EXTRA_PARAM_PM25);
+                final String param_pm10 = intent.getStringExtra(EXTRA_PARAM_PM10);
+                final String param_wifidb = intent.getStringExtra(EXTRA_PARAM_WIFIDB);
+                final String param_wifipercent = intent.getStringExtra(EXTRA_PARAM_WIFIPERCENT);
+
+                TextView textview = (TextView) findViewById(R.id.pm25_textview);
+                textview.setText(param_pm25);
+
+                textview = (TextView) findViewById(R.id.pm10_textview);
+                textview.setText(param_pm10);
+
+                textview = (TextView) findViewById(R.id.wifidb_textview);
+                textview.setText(param_wifidb);
+
+                textview = (TextView) findViewById(R.id.wifipercent_textview);
+                textview.setText(param_wifipercent);
+            }
+        }
+    };
+
+    public static String[] querySensorValues() {
+        String s[] = {"0","0","0","0"};
+        String tmp = "";
+
+        /*
+         * compile the regular expression to be ready to run
+         * */
+        String pm25Regex = "(PM2.5)[a-z<>\\/ \\=\\']*([0-9\\.]+)+";
+        String pm10Regex = "(PM10)[a-z<>\\/ \\=\\']*([0-9\\.]+)+";
+        String wifidbRegex = "(Signal)(?:[<>\\/a-z \\=']+)([0-9\\.-]+)";
+        String wifipercentRegex = "(Qualität)(?:[<>\\/a-z \\=']+)([0-9\\.-]+)";
+        Pattern pm25Pattern = Pattern.compile(pm25Regex);
+        Pattern pm10Pattern = Pattern.compile(pm10Regex);
+        Pattern wifidbPattern = Pattern.compile(wifidbRegex);
+        Pattern wifipercentPattern = Pattern.compile(wifipercentRegex);
+        /*
+         * do the HTTP request and download the
+         */
+        Request request = new Request.Builder()
+                .url("http://192.168.10.1/values")
+                .build();
+        try {
+            Response response = client.newCall(request).execute();
+            if (response.isSuccessful()) {
+                tmp = response.body().string();
+                Log.d("MainActivity",tmp);
+                response.close();
+            }
+        } catch (IOException ioex) {
+            Log.wtf("MainActivity",ioex);
+            Log.getStackTraceString(ioex);
+            tmp = "";
+        }
+
+        Matcher m = pm25Pattern.matcher(tmp);
+        if(m.find()){
+            s[0] = m.group(2);
+        }
+
+        m = pm10Pattern.matcher(tmp);
+        if(m.find()){
+            s[1] = m.group(2);
+        }
+
+        m = wifidbPattern.matcher(tmp);
+        if(m.find()){
+            s[2] = m.group(2);
+        }
+
+        m = wifipercentPattern.matcher(tmp);
+        if(m.find()){
+            s[2] = m.group(2);
+        }
+
+        return s;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        client = new OkHttpClient();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(MyIntentService.BROADCAST_ACTION_BAZ);
+        LocalBroadcastManager bm = LocalBroadcastManager.getInstance(this);
+        bm.registerReceiver(mBroadcastReceiver, filter);
+
+        downcounter = new CountDownTimer(Long.MAX_VALUE, 70000) {
+            @Override
+            public void onTick(long l) {
+                Log.d("DownCounter",String.valueOf(l));
+                MyIntentService.startActionFoo(MainActivity.this, "test2");
+            }
+
+            @Override
+            public void onFinish() {
+                Log.d("DownCounter","finished");
+                start();
+            }
+        }.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        LocalBroadcastManager bm = LocalBroadcastManager.getInstance(this);
+        bm.unregisterReceiver(mBroadcastReceiver);
+        client = null;
+        super.onDestroy();
     }
 
     public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editText);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
 
-        Intent startServiceIntent = new Intent(this, MyJobService.class);
-        startService(startServiceIntent);
     }
-    /**
-     * Called when the user taps the Send button
-     */
-    public void sendMessage2() {
-        String s = "";
-        EditText editText = (EditText) findViewById(R.id.editText);
 
-        /*
-         * compile the regular expression to be ready to run
-         * */
-        String patternStr = "Windows\\s([0-9]+\\.[0-9]+\\.[0-9]+)";
-        Pattern p = Pattern.compile(patternStr);
-
-        /*
-         * do the HTTP request and download the
-         */
-        Request request = new Request.Builder()
-                .url("http://192.168.43.143/dashboard/index.html")
-                .build();
-        try {
-            Response response = client.newCall(request).execute();
-            if(response.isSuccessful())
-            {
-                response.close();
-                s = response.body().string();
-            }
-
-        } catch (IOException ioex) {
-            s = ioex.getMessage();
-            s = "bar";
-        }
-
-        /*
-         * do the regex matching an extract the caputre group
-         * */
-        //Matcher m = p.matcher(s);
-
-        /*
-         * if we found something, display it.
-         * else, display the "error message"
-         *  */
-        editText.setText(s);
-//        if (m.find()) {
-//            editText.setText(m.group(1));
-//        } else {
-//            editText.setText(String.format("Nothing found %d", ++counter));
-//    }
-        }
+    // send data to MyService
+    protected void communicateToService(String parameter) {
+        MyIntentService.startActionFoo(this, parameter);
     }
+}
---
+++ app/src/main/java/com/example/m/myfirstapp/MyIntentService.java
@@ -0,0 +1,62 @@
+package com.example.m.myfirstapp;
+
+import android.app.IntentService;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+public class MyIntentService extends IntentService {
+    private static final String ACTION_FOO = "com.myapp.action.FOO";
+    private static final String EXTRA_PARAM_A = "com.myapp.extra.PARAM_A";
+
+    public static final String BROADCAST_ACTION_BAZ = "com.myapp.broadcast_action.FOO";
+    public static final String EXTRA_PARAM_PM25 = "com.myapp.extra.PARAM_PM25";
+    public static final String EXTRA_PARAM_PM10 = "com.myapp.extra.PARAM_PM10";
+    public static final String EXTRA_PARAM_WIFIDB = "com.myapp.extra.PARAM_WIFIDB";
+    public static final String EXTRA_PARAM_WIFIPERCENT = "com.myapp.extra.PARAM_WIFIPERCENT";
+
+
+
+    // called by activity to communicate to service
+    public static void startActionFoo(Context context, String param1) {
+        Intent intent = new Intent(context, MyIntentService.class);
+        intent.setAction(ACTION_FOO);
+        intent.putExtra(EXTRA_PARAM_A, param1);
+        context.startService(intent);
+    }
+
+    public MyIntentService() {
+
+        super("MyIntentService");
+
+
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        String queryresult[];
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (ACTION_FOO.equals(action)) {
+                final String param1 = intent.getStringExtra(EXTRA_PARAM_A);
+                Log.d("MyIntentService",param1);
+                queryresult = MainActivity.querySensorValues();
+
+                broadcastActionBaz(queryresult);
+            }
+        }
+    }
+
+    // called to send data to Activity
+    public void broadcastActionBaz(String params[]) {
+        Intent intent = new Intent(BROADCAST_ACTION_BAZ);
+        intent.putExtra(EXTRA_PARAM_PM25, params[0]);
+        intent.putExtra(EXTRA_PARAM_PM10, params[1]);
+        intent.putExtra(EXTRA_PARAM_WIFIDB, params[2]);
+        intent.putExtra(EXTRA_PARAM_WIFIPERCENT, params[3]);
+
+        LocalBroadcastManager bm = LocalBroadcastManager.getInstance(this);
+        bm.sendBroadcast(intent);
+    }
+}
