6e29b4c7fcf82cc27252d82de56c3f335c72050a
MJJoker <ziebell_marco@posteo.de>
2018-03-09 11:56:36 +0100

    - UI OverallV1#1

---
+++ app/src/main/java/com/example/m/myfirstapp/DeviceOverview.java
@@ -0,0 +1,13 @@
+package com.example.m.myfirstapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class DeviceOverview extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_overview);
+    }
+}
